37252458a43d4bffd3a3f312c916f871c56b8730
Yash_Gadhwe <89290742+YashSMG@users.noreply.github.com>
2024-03-15 19:03:19 +0530

    Delete fullscreen_overlay_nav_menu/fullscreen_overlay_nav_menu directory

--- fullscreen_overlay_nav_menu/fullscreen_overlay_nav_menu/script.js
+++
@@ -1,8 +0,0 @@
-// Toggle overlay menu
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'block';
-});
-
-document.getElementById('overlay').addEventListener('click', function() {
-    this.style.display = 'none';
-});
